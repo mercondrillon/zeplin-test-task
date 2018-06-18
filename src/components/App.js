6d350233fd316559e34423f6a0c82fb88a03d795
Mer Condrillon <mercondrillon.work@gmail.com>
2018-06-18 13:29:01 +0800

    Move base router wrapper to index

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,13 @@ import 'antd/dist/antd.css';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { assignNotes, assignCodes } from '../actions';
+import { Route } from  'react-router-dom';
 
 import ClientForm from './ClientForm';
 import NoteList from './NoteList';
 
-import { BrowserRouter as Router, Route } from  'react-router-dom';
 import API from '../services/http';
+
 import Navigation from './Navigation/Navigation';
 import LoadingBar from 'react-redux-loading-bar';
 import { Layout } from 'antd';
@@ -37,27 +38,25 @@ class AppComponent extends Component {
 
   render() {
     return (
-      <Router>
-        <Layout style={{ minHeight: '100vh' }}>
-          <Sider
-            collapsible
-            collapsed={this.state.collapsed}
-            onCollapse={this.onCollapse}
-          >
-            <div className="logo" />
-            <Navigation />
-          </Sider>
-          <Header style={{ background: '#fff', padding: 0 }}>
-            <LoadingBar style={{ backgroundColor: '#1890ff', height: '5px' }}/>
-          </Header>
-          <Content style={{ margin: '5px 16px' }}>
-            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-              <Route path={'/notes-create'} component={ClientForm} />
-              <Route path={'/notes'} component={NoteList} />
-            </div>
-          </Content>
-        </Layout>
-      </Router>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider
+          collapsible
+          collapsed={this.state.collapsed}
+          onCollapse={this.onCollapse}
+        >
+          <div className="logo" />
+          <Navigation />
+        </Sider>
+        <Header style={{ background: '#fff', padding: 0 }}>
+          <LoadingBar style={{ backgroundColor: '#1890ff', height: '5px' }}/>
+        </Header>
+        <Content style={{ margin: '5px 16px' }}>
+          <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+            <Route path={'/notes-create'} component={ClientForm} />
+            <Route path={'/notes'} component={NoteList} />
+          </div>
+        </Content>
+      </Layout>
     );
   }
 }
