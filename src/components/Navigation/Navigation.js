95bcb352ca426848bcf9815d0bc0d3d933f532a4
Mer Condrillon <mercondrillon.work@gmail.com>
2018-06-18 09:04:07 +0800

    router baseurl

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom';
 const Navigation = () => (
   <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
     <Menu.Item key="1">
-      <Link to="/notes">
+      <Link to="/zeplin-test-task/notes">
         <Icon type="desktop" />
         Notes
       </Link>
     </Menu.Item>
     <Menu.Item key="2">
-      <Link to="/notes-create">
+      <Link to="/zeplin-test-task/notes-create">
         <Icon type="file-add" />
         Create Note
       </Link>
